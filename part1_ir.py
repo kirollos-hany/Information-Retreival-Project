163b2dc038aca7be69784bb6197f55501bf2f452
= <=>
2021-12-10 12:04:16 +0200

    first part

---
+++ part1_ir.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 10 08:40:10 2021
+
+@author: Malak-pc
+"""
+
+#import nltk
+#nltk.download('punkt')
+#nltk.download('stopwords')
+#nltk.download('wordnet')
+
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+
+stop_words      = stopwords.words('english')
+porter_stemmer  = PorterStemmer()
+word_lemmitizer = WordNetLemmatizer()
+
+#function to read file and return its data
+def read_file(path):
+    f = open(path, "r")
+    data=f.read()
+    f.close()
+    return data
+
+#read documents
+
+documents={
+          "doc1"  : read_file("doc1.txt"),
+          "doc2"  : read_file("doc2.txt"),
+          "doc3"  : read_file("doc3.txt"),
+          "doc4"  : read_file("doc4.txt"),
+          "doc5"  : read_file("doc5.txt"),
+          "doc6"  : read_file("doc6.txt"),
+          "doc7"  : read_file("doc7.txt"),
+          "doc8"  : read_file("doc8.txt"),
+          "doc9"  : read_file("doc9.txt"),
+          "doc10" : read_file("doc10.txt")
+}
+punctuations="?:!.,;"
+
+tokenized_documents={}
+
+for key, value in documents.items():
+    tokenized_documents[key]= word_tokenize(value) #tokenize documents
+    tokenized_documents[key]= [t for t in tokenized_documents[key] if not t in stop_words] #remove stop words
+    tokenized_documents[key]= [porter_stemmer.stem(t) for t in tokenized_documents[key]] #apply porter stemming
+    tokenized_documents[key]= [word_lemmitizer.lemmatize(t) for t in tokenized_documents[key]] #apply lemmitizer
+    tokenized_documents[key]= [t for t in tokenized_documents[key] if not t in punctuations] #remove punctuations
+
+
+print(tokenized_documents)
